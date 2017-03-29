e321195c96d6a92b14c14871d6a314646cc7d6e7
Amanda Munch <amandajmunch@gmail.com>
2017-03-29 19:39:05 -0400

    successful API call, need to render art on page

---
+++ apartment2/app/controllers/search_arts_controller.rb
@@ -0,0 +1,13 @@
+class SearchArtsController < ApplicationController
+
+  def keyword
+    # searches the database for art
+    # @user = User.find(params[:id])
+    @arts = HTTParty.get("https://www.rijksmuseum.nl/api/en/collection?key=RG3BxRnZ&format=json&q=#{params[:art][:title]}&imgonly=true")
+    puts @arts.to_json
+    @art = Art.new
+    json = JSON.parse(response.body)
+  end
+end
+
+
