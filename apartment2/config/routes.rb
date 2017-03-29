e5488d2e0c5cc40b8d38dc970168652a9f72b5b9
Amanda Munch <amandajmunch@gmail.com>
2017-03-29 17:21:21 -0400

    updated controller

--- apartment2/config/routes.rb
+++ apartment2/config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root to: "home#index"
+  root to: "users#index"
 
+  get "/profile", to: "users#profile", as: :arts
+  get "/profile/", to: "users#index", as: :user
+  get "/profile/", to: "users#new", as: :new_art
+  post "/profile/", to: "users#create", as: :create_art
+
+def index
+    # @user = User.find(params[:id])
+    @arts = Art.all
+
+    # @arts = @user.arts
+  end
 end
