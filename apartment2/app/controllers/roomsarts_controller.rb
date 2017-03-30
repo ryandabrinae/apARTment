a54be64c156ee3c1f37dda36d4e44f73571381e1
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-03-30 15:10:21 -0400

    shows single room art pairing

--- apartment2/app/controllers/roomsarts_controller.rb
+++ apartment2/app/controllers/roomsarts_controller.rb
@@ -1,4 +1,8 @@
 class RoomsartsController < ApplicationController
+    def show
+        @roomsart = RoomsArt.find(params[:id])
+    end
+
     def create
         @roomsart = RoomsArt.new
         @roomsart.art_id = params[:rooms_art][:art]
@@ -6,7 +10,7 @@ class RoomsartsController < ApplicationController
         @roomsart.room_id = room_id
 
         if @roomsart.save
-            redirect_to "/profile/rooms/#{room_id}"
+            redirect_to @roomsart
         else
             redirect_back fallback_location: root_path
         end
