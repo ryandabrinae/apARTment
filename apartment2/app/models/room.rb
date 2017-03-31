5dec3bc77f48653533741be81fe54233a53f0290
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-03-30 20:25:25 -0400

    can update and delete rooms, room names are validated based on uniqueness to user

--- apartment2/app/models/room.rb
+++ apartment2/app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   validates :type_of_room, presence: true
   validates :name, presence: true
   validates :user, presence: true
+  validates_uniqueness_of :name, scope: :user_id
   validates :status, presence: true
   
   belongs_to :user
