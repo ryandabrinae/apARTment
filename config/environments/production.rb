b2da761ef6226701b6bdc02d363f5db077f98e35
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-04-03 12:57:38 -0400

    iframe test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
