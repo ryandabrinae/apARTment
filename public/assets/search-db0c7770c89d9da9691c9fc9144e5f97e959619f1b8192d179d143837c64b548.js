1a9b2cf2976f85922f2b53d6af1be5230de74323
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-04-03 12:08:49 -0400

    precompile

---
+++ public/assets/search-db0c7770c89d9da9691c9fc9144e5f97e959619f1b8192d179d143837c64b548.js
@@ -0,0 +1,23 @@
+(function() {
+  $(function() {
+    return $(".search").on("ajax:success", function(e, data, status, xhr) {
+      var i, image, imageSrc, j, len, pieceHolder, ref, results, saveForm, title, titleHolder, titleLink;
+      ref = data.artObjects;
+      results = [];
+      for (j = 0, len = ref.length; j < len; j++) {
+        i = ref[j];
+        pieceHolder = $('<div class = "artList">');
+        imageSrc = "" + i.webImage.url;
+        image = $('<img>').attr('src', imageSrc);
+        titleHolder = "" + i.title;
+        titleLink = $('<a>').text(titleHolder).attr('href', "search/" + i.objectNumber);
+        title = $('<h3>').append(titleLink);
+        saveForm = $('<form>');
+        pieceHolder.append(title, image);
+        results.push($('.search-results').append(pieceHolder));
+      }
+      return results;
+    });
+  });
+
+}).call(this);
